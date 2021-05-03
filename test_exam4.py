65a8eb53791e4213c184f62d9982a619cf5aecc9
mjgregoire <81594574+mjgregoire@users.noreply.github.com>
2021-05-03 15:36:20 -0400

    updated test_exam4.py

--- test_exam4.py
+++ test_exam4.py
@@ -1,6 +1,9 @@
 from exam4 import *
 
-def test_count_kmers_observed(read, k):
+read = "ATTTGGATT"
+k = 3
+
+def test_count_kmers_observed():
   '''
   This function is used to test the count kmers of size k function. 
   '''
@@ -9,7 +12,7 @@ def test_count_kmers_observed(read, k):
   assert actual_result == expected_result
   
 
-def test_count_kmers_possible(read, k):
+def test_count_kmers_possible():
   '''
   This function will test the count the number of all possible kmers function.
   '''
@@ -18,20 +21,21 @@ def test_count_kmers_possible(read, k):
   assert actual_result == expected_result
 
   
-def test_create_panda(read):
+def test_create_panda():
   '''
   This function creates the pandas dataframe for the number of k, observed kmers, and possible kmers
   '''
   actual_result = create_panda(read)
-  expected_result_df = ({'k': [1,2,3,4,5,6,7,8,9],'observed kmers': [3,5,6,6,5,4,3,2,1,35], 'possible kmers':[4,8,7,6,5,4,3,2,1,40]})
-  assert actual_result_df == expected_result_df
+  expected_result = ({'k': [1,2,3,4,5,6,7,8,9],'observed kmers': [3,5,6,6,5,4,3,2,1,35], 'possible kmers':[4,8,7,6,5,4,3,2,1,40]})
+  assert actual_result == expected_result
 
 
 
-def test_calculate_LC(read):
+def test_calculate_LC():
   '''
   This function tests the function to calculate total linguistic complexity 
   '''
   actual_result = calculate_LC(read)
-  expected_LC = 0.875
-  assert actual_LC == expected_LC
+  expected_result = 0.875
+  assert actual_result == expected_result
+ 
